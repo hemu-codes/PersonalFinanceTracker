a17180512c642343c957676b0ec1c0740ba5dbf5
hemu-codes <akkarh2@uw.edu>
2026-02-03 11:29:26 -0800

    Initial commit: Spring Boot + React Personal Finance App setup

---
+++ src/test/java/com/hemu_codes/PersonalFinanceTracker/PersonalFinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemu_codes.PersonalFinanceTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
